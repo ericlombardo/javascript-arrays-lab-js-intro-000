42ab98f380b2a9c27048b1a555735facbbeb8b1d
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-12 19:47:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(newKit) {
-  appendKittens(newKit);
+  kittens.push(newKit);
 }
